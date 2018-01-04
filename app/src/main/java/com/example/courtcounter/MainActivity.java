4bf31d525b634f5903a5b93f68eb8df879458119
ChocolateCharlie <aurore.amrit@gmail.com>
2018-01-04 11:38:03 +0100

    Setting up the Methods (step 21)

--- app/src/main/java/com/example/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/courtcounter/MainActivity.java
@@ -1,13 +1,80 @@
+/**
+ * MainActivity.java
+ * By : ChocolateCharlie
+ * Last Updates : 04 - 01 - 2018
+ */
+
 package com.example.courtcounter;
+
+
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import com.example.courtcounter.R;
+
+
+
+/**
+ * MainActivity class
+ * @author ChocolateCharlie
+ *
+ * React to button pressing.
+ */
 public class MainActivity extends AppCompatActivity {
 
+/**
+ * onCreate
+ * @param savedInstanceState
+ *
+ * This method is called at the creation of the class.
+ */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+/**
+ * displayForTeamA
+ * @param score
+ *
+ * Displays the given score for Team A.
+ */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+/**
+ * addOneForTeamA
+ * @param view
+ *
+ * Display 1
+ */
+    public void addOneForTeamA (View view) {
+        displayForTeamA(1);
+    }
+
+/**
+ * addTwoForTeamA
+ * @param view
+ *
+ * Display 2
+ */
+    public void addTwoForTeamA (View view) {
+        displayForTeamA(2);
+    }
+
+
+/**
+ * addThreeForTeamA
+ * @param view
+ * Display 3
+ */
+    public void addThreeForTeamA (View view) {
+        displayForTeamA(3);
     }
 }
